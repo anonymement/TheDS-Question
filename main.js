5aa1716e9c2a695e804d44eeb4493f27edb34a3a
anonymement <jatochh12@gmail.com>
2024-11-15 15:40:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-        var password = "dualeoverzanten";
+var password = "dualeoverzanten";
 
-        function passcheck(){
-            if(document.getElementById('pass1').value != password) {
-                alert('Verkeerde wachtwoord, probeer nogmaals.')
-                return false;
-            }
-            if(document.getElementById('pass1').value == password) {
-            }
-        }
+function passcheck(){
+    if(document.getElementById('pass1').value != password) {
+        alert('Verkeerde wachtwoord, probeer nogmaals.')
+        return false;
+    }
+    if(document.getElementById('pass1').value == password) {
+    }
+}
